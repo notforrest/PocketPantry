bda6211aef8e9eb8eb4560eef840e728d4efac70
Kacey Na <kacey.na01@gmail.com>
2024-05-06 03:36:50 -0500

    Added basic elements to profile page

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,8 +1,9 @@
 import { router } from "expo-router";
 import React from "react";
 import { StyleSheet, Text, View, TouchableOpacity, Image } from "react-native";
-import { Button } from "react-native-elements";
+import { Button, Icon } from "react-native-elements";
 import { Theme, useTheme } from "../../utils/ThemeProvider";
+import { Ionicons } from "@expo/vector-icons";
 
 export default function HomePage() {
   const styles = getStyles(useTheme());
@@ -10,11 +11,30 @@ export default function HomePage() {
 
   return (
     <View style={styles.container}>
+      <TouchableOpacity
+        style={{ position: "absolute", right: "10%", top: "9%", zIndex: 1 }}
+      >
+        <Ionicons name="create-outline" size={32} color="black" />
+      </TouchableOpacity>
       <Text style={styles.title}>My Profile</Text>
-      <Image source={require(defaultProfile)} style={styles.profilePicture} />
+      {/*<Image source={require(defaultProfile)} style={styles.profilePicture} />*/}
+      <Ionicons name="person-circle" size={150} />
       <Text style={styles.profileHeader}>Username</Text>
       <View style={styles.profileBody}>
-        <Text>TEST TEST TEST TEST</Text>
+        <Ionicons name="person-circle" size={32} />
+        <Text style={styles.profileText}> Bio...</Text>
+      </View>
+      <View style={styles.profileBody}>
+        <Ionicons name="mail-outline" size={32} />
+        <Text style={styles.profileText}> user@email.com</Text>
+      </View>
+      <View style={styles.profileBody}>
+        <Ionicons name="call-outline" size={32} />
+        <Text style={styles.profileText}> (123) 456-7890</Text>
+      </View>
+      <View style={styles.profileBody}>
+        <Ionicons name="calendar-outline" size={32} />
+        <Text style={styles.profileText}> 11/24/2001</Text>
       </View>
     </View>
   );
@@ -27,11 +47,13 @@ const getStyles = (theme: Theme) =>
       backgroundColor: theme.background,
       flex: 1,
       justifyContent: "center",
+      padding: 20,
     },
     title: {
       fontSize: 40,
       textAlign: "center",
       fontWeight: "bold",
+      marginTop: 20,
       marginBottom: 20,
       width: "80%",
     },
@@ -55,11 +77,23 @@ const getStyles = (theme: Theme) =>
     },
     profileHeader: {
       fontSize: 30,
+      marginBottom: 20,
     },
     profileText: {
       fontSize: 20,
     },
     profileBody: {
-      alignItems: "flex-end",
+      backgroundColor: "white",
+      height: 70,
+      width: 350,
+      padding: 20,
+      borderTopLeftRadius: 50,
+      borderTopRightRadius: 50,
+      borderBottomLeftRadius: 50,
+      borderBottomRightRadius: 50,
+      justifyContent: "flex-start",
+      alignItems: "stretch",
+      marginTop: 20,
+      flexDirection: "row",
     },
   });
